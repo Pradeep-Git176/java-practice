fea91c338dfb837d26fe2cac5eb3b99a23bff7ef
Pradeep-Git176 <96973824+Pradeep-Git176@users.noreply.github.com>
2023-05-25 16:49:18 +0530

    Add files via upload
    join two java strings

---
+++ may/JoinTwoJavaStrings.java
@@ -0,0 +1,20 @@
+package may;
+
+// using concat()
+public class JoinTwoJavaStrings {
+	public static void main(String[] args) {
+
+	    // create first string
+	    String first = "Pradeep ";
+	    System.out.println("First String: " + first);
+
+	    //second string
+	    String second = "kumar";
+	    System.out.println("Second String: " + second);
+
+	    // join two strings
+	    String joinedString = first.concat(second);
+	    System.out.println("Joined String: " + joinedString);
+	  }
+	}
+
