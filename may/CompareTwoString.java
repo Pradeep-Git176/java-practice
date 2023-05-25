119ac01682d9d4ecedd14458e2ae1b6f4aa89ac9
Pradeep-Git176 <96973824+Pradeep-Git176@users.noreply.github.com>
2023-05-25 16:51:41 +0530

    compare two strings

---
+++ may/CompareTwoString.java
@@ -0,0 +1,21 @@
+package may;
+
+public class CompareTwoString {
+	public static void main(String[] args) {
+
+	    // create 3 strings
+	    String first = "Pradeep kumar";
+	    String second = "Pradeep kumar";
+	    String third = "Rajesh sah";
+
+	    // compare first and second strings
+	    boolean result1 = first.equals(second);
+	    System.out.println("Strings first and second are equal: " + result1);
+
+	    // compare first and third strings
+	    boolean result2 = first.equals(third);
+	    System.out.println("Strings first and third are equal: " + result2);
+	  }
+	}
+
+
